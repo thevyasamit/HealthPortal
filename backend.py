56d384be42057b5736ea614c571390361c4a1bd3
Amit Vyas <23amitvyas@gmail.com>
2021-01-11 16:46:44 +0530

    Changes

--- backend.py
+++
@@ -1,7 +0,0 @@
-# from routes import app
-# from flask import Flask
-# from flaskCode.models import User
-
-# @app.route('/user/data', methods= ['POST'])
-# def signup():
-# 	return User().signup()
--- routes.py
+++ routes.py
@@ -14,8 +14,6 @@ db = client.healthDB
 
 
 
-
-
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
@@ -46,8 +44,6 @@ def userData():
 		b = ser.readline()         
 		string = b.decode()  
 		string = string.rstrip()
-    	#flt = float(string)        
-    	#print(string)
 		sensorData.append(string)          
 		time.sleep(1)            
 	ser.close()
@@ -68,9 +64,6 @@ def userData():
 	                rate=RATE,
 	                input=True,
 	                frames_per_buffer=CHUNK)
-
-	print("* recording")
-
 	frames = []
 
 	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
@@ -84,14 +77,12 @@ def userData():
 	p.terminate()
 
 	wf = wave.open(r"/Users/avyas/Desktop/teleMed/flaskDev/flaskCode/static/audio/"+WAVE_OUTPUT_FILENAME,"wb")
-	#wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 	wf.setnchannels(CHANNELS)
 	wf.setsampwidth(p.get_sample_size(FORMAT))
 	wf.setframerate(RATE)
 	wf.writeframes(b''.join(frames))
 	wf.close()
 
-	# return sensorData
 	user = {
 		"_id" : iD,
 		"name": request.form.get('name'),
@@ -110,8 +101,6 @@ def userData():
 def usr_data():
 	data = []
 	data = db.users.distinct('contact')
-	#data = db.users.find({} , { "name" : 1 , "_id" : 0})
-	#data = db.users.find()
 	return render_template('data.html',data = data)
 
 
@@ -122,15 +111,8 @@ def info():
 		data = []
 		con =request.form.get('val')
 		data = db.users.find({ "contact" : con }, {"_id" : 1, "name" : 1, "contact" : 1 , "age" : 1, "sensor" : 1 , "audio" :1})
-		# print(con)
-		# nm =request.form.get('val2')
-		# data = db.users.find({ "name" : nm }, {"_id" : 1, "name" : 1, "contact" : 1 , "age" : 1, "sensor" : 1})
-		
-		# print(nm)
 		return render_template('recieve.html', data = data)
 	else:
-		# gname = request.args.get('nm')
-		# #print(gname)
 		return render_template('recieve.html')
 
 
@@ -164,39 +146,6 @@ def filename(filename):
 	p.terminate()  
 	return ('', 204)
 
-# @app.route("/play_audio/", methods = ['POST', 'GET'])
-# def play_audio():
-# 	if request.method == 'POST':
-# 		f = request.form.get('audio')
-# 	else:
-# 		f = request.args.get('audio')
-	# chunk = 1024  
-	# audioFname= '59ffc94a-4dfd-4012-a24e-97906d5609f4.wav'
-	# #open a wav format music  
-	# f = data#wave.open(r"/users/avyas/Desktop/teleMed/flaskDev/flaskCode/"+audioFname,"rb")  
-	# #instantiate PyAudio  
-	# p = pyaudio.PyAudio()  
-	# #open stream  
-	# stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
-	#             channels = f.getnchannels(),  
-	#             rate = f.getframerate(),  
-	#             output = True)  
-	# #read data  
-	# data = f.readframes(chunk)  
-
-	# #play stream  
-	# while data:  
-	# 	stream.write(data)  
-	# 	data = f.readframes(chunk)  
-
-	# #stop stream  
-	# stream.stop_stream()  
-	# stream.close()  
-
-	# #close PyAudio  
-	# p.terminate()
-	# 	print(f)
-	# return render_template()
 
 @app.route("/recieve/")
 def recieve():
@@ -208,10 +157,6 @@ if __name__ == "__main__":
 
 
 
-# - tell isp to open port
-# - portname :tcp 80 443 3478 3479 3480
-# 			udp 3478 3479
-# - Reliance 
 
 # In order to run this app uisng environment variable use 
 # Use this command in terminal: 
@@ -219,7 +164,7 @@ if __name__ == "__main__":
 # in order to not run server again and again use export FLASK_DEBUG =1 or in app.run(debug = true)
 # export FLASK_ENV=development worked for enabling debug mode in my case
 
-#MongoDB commands
+#MongoDB commands for MAC OS
 #brew services start mongodb-community
 #brew services stop mongodb-community
 
